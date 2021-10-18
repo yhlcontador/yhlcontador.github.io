3ef86a10f1274748f216f5602df9c14e4a156304
YHLCONTADORES <92733949+YHLCONTADORES@users.noreply.github.com>
2021-10-18 08:58:19 -0500

    YHLCONTADORESPAGE

---
+++ enviar.php
@@ -0,0 +1,29 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'src/Exception.php';
+require 'src/PHPMailer.php';
+require 'src/SMTP.php';
+
+$nombre = $_POST['nombre'];
+$mail = $_POST['email'];
+$mensaje = $_POST['mensaje'];
+
+$header = 'From: ' . $mail . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
+$mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
+$mensaje .= "Su e-mail es: " . $mail . " \r\n";
+$mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
+$mensaje .= "Enviado el " . date('d/m/Y', time());
+
+$para = 'yhlcontadores@gmail.com';
+$asunto = 'CONSULTA';
+
+mail($para, $asunto, utf8_decode($mensaje), $header);
+
+header("Location:index.html");
+?>

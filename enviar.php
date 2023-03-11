762f83d6d224ea8d48a9595aed1490bad286b600
YHLCONTADORES <92733949+YHLCONTADORES@users.noreply.github.com>
2023-03-11 18:01:15 -0500

    Update enviar.php

--- enviar.php
+++ enviar.php
@@ -1,8 +1,6 @@
 <?php
 $name = $_POST['name'];
 $mail = $_POST['mail'];
-$phone = $_POST['phone'];
-$message = $_POST['message'];
 
 $header = 'From: ' . $mail . " \r\n";
 $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
@@ -11,7 +9,6 @@ $header .= "Content-Type: text/plain";
 
 $message = "Este mensaje fue enviado por: " . $name . " \r\n";
 $message .= "Su e-mail es: " . $mail . " \r\n";
-$message .= "Teléfono de contacto: " . $phone . " \r\n";
 $message .= "Mensaje: " . $_POST['message'] . " \r\n";
 $message .= "Enviado el: " . date('d/m/Y', time());
 
